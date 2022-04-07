6fa8771b550ec2080d0347eee0b6bfc54c84f2e3
mkaraca25 <61596919+mkaraca25@users.noreply.github.com>
2022-04-07 16:51:42 +0300

    Database connected...

--- mkaraca/src/main/java/labApp/mkaraca/dataAccess/abstracts/ReportDescriptionDao.java
+++
@@ -1,9 +0,0 @@
-package labApp.mkaraca.dataAccess.abstracts;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import labApp.mkaraca.entities.concretes.ReportDescription;
-
-public interface ReportDescriptionDao extends JpaRepository<ReportDescription, Integer>{
-
-}
